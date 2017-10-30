a2e8aede59749edeac082e94df1d193ab337f213
NIJIN SEBASTIAN <kirangplkrishnan@gmail.com>
2017-10-30 01:22:42 -0400

    Enter key mapped to =

--- js/calculator.js
+++ js/calculator.js
@@ -1,70 +1,73 @@
-(function(){
-  /*
-  *TODO : Disable use strict in production code
-  */
-  'use strict';
-  //STACK implemented as a constructor function
-  function Stack(){
-    this.stack=[];
-    this.push = function(item){
-      this.stack.push(item);
-    }
-    this.size = function(){
-      return this.stack.length;
-    }
-    this.pop = function(){
-      return this.stack.pop();
-    }
+//Not enclosing in a ASIF because the main.js requires the caclulator object
+'use strict';
+//Stack implemented as a constructor function
+function Stack(){
+  this.stack=[];
+  this.push = function(item){
+    this.stack.push(item);
   }
+  this.size = function(){
+    return this.stack.length;
+  }
+  this.pop = function(){
+    return this.stack.pop();
+  }
+}
 
-  function Calculator() {
-    this.opList=["*","/","+","-"],
-    //Characters allowed on the calculator
-    this.allowedCharacters=["0","1","2","3","4","5","6","7","8","9",".","(",")"].concat(this.opList),
-    this.getAllowedCharacters = function(){
-      return this.allowedCharacters;
-    }
-    this.decimalPrecision=5,
-    this.setDecimalPrecision=function(number){
-      this.decimalPrecision = number;
+function Calculator() {
+  this.opList=["*","/","+","-"],
+  //Characters allowed on the calculator
+  this.allowedCharacters=["0","1","2","3","4","5","6","7","8","9",".","(",")"].concat(this.opList),
+  this.getAllowedCharacters = function(){
+    return this.allowedCharacters;
+  }
+  this.decimalPrecision=5,
+  this.setDecimalPrecision=function(number){
+    this.decimalPrecision = number;
+  },
+  this.opFunctions=[
+    //Same presidence operators are grouped together in left to right order
+    {
+      '*': function(a, b){ return a * b},
+      '/': function(a, b){ return a / b}
     },
-    this.opFunctions=[
-      //Same presidence operators are grouped together in left to right order
-      {
-        '*': function(a, b){ return a * b},
-       '/': function(a, b){ return a / b}
-     },
-      {
-        '+': function(a, b){ return a + b},
-         '-': function(a, b){ return a - b}
-      }
-    ]
-  }
-
-  Calculator.prototype.calculate = function(string){
-      var stack = new Stack();
-      var generatedOpStack = this.generateOpStack(string,stack);
-      var calculatedOutput = this.calculateStackItems(generatedOpStack);
-      //setting precision
-      calculatedOutput = calculatedOutput.toFixed(this.decimalPrecision);
-      return calculatedOutput;
+    {
+      '+': function(a, b){ return a + b},
+      '-': function(a, b){ return a - b}
     }
+  ]
+}
 
-  Calculator.prototype.generateOpStack = function(string,stackObj){
-    var stack = stackObj;
-    var array = string.split("");
-    var tempArray = [];
-    let selfe = this;
-    array.forEach(function(item){
-      if(item === ')'){
-        var tempArray=[];
-        var nextItem= stack.pop();
-        //console.log(nextItem);
-        while(nextItem !== '('){
-          tempArray.push(nextItem);
-          nextItem= stack.pop();
-        }
-        //reverse the array because we are going backwords in the while loop
+Calculator.prototype.calculate = function(string){
+  var response;
+  var stack = new Stack();
+  var error = this.checkError(string);
+if(!error){
+    var generatedOpStack = this.generateOpStack(string,stack);
+    response = this.calculateStackItems(generatedOpStack);
+  }else{
+    response = "Error";
+  }
+  console.log(error);
+  //calculatedOutput = calculatedOutput.toFixed(this.decimalPrecision);
+  return response;
+}
+
+Calculator.prototype.generateOpStack = function(string,stackObj){
+  var stack = stackObj;
+  var array = string.split("");
+  let selfe = this;
+  array.forEach(function(item){
+    if(item === ')'){
+      //keep popping items out of stack until the matching opening bracket is found
+      var tempArray=[];
+      var nextItem= stack.pop();
+
+      while(nextItem !== '('){
+        tempArray.push(nextItem);
+        nextItem= stack.pop();
+      }
+      //reverseing the array to ensure operation order is kept as the original string
       var bracketString = tempArray.reverse().join("");
       var parsedOperationArray = selfe.parseOperationString(bracketString);
       var clearedOperationArray = selfe.clearArray(parsedOperationArray);
@@ -76,104 +79,116 @@
       //console.log(output);
       var result = output;
       stack.push(result);
-      }else{
-        stack.push(item);
-      }
-    });
-    //returning the filled stack
+    }else{
+      stack.push(item);
+    }
+  });
+  //returning the filled stack
 
-    console.log(stack.stack);
-    return stack;
-  }
-  var addToScreen = function(currentScreenValue,clickedButtonValue){
-    return currentScreenValue+clickedButtonValue;
-  }
-  Calculator.prototype.calculateStackItems= function(stack){
-    let currentStack = stack;
-    let tempArray = [];
-    var stackSize = stack.size();
-    //emptying the stack
-    while(stackSize>0){
-      tempArray.push(currentStack.pop());
-      stackSize = stack.size();
-    }
-    //joining the stack items together for parsing
-    var stackOpString = tempArray.reverse().join("");
-    var parsedOperationArray = this.parseOperationString(stackOpString);
-    var output = this.performOperations(parsedOperationArray);
-    return output;
-  }
-  Calculator.prototype.performOperations= function(array){
-    //getting the operation functions from the calculator object
-    var opList = this.opFunctions;
-    var newArray = [];
-    var currentOperator;
-    for (var i = 0; i < opList.length; i++) {
-        for (var j = 0; j < array.length; j++) {
-            if (opList[i][array[j]]) {
-                currentOperator = opList[i][array[j]];
-            } else if (currentOperator) {
-                newArray[newArray.length - 1] =
-                    currentOperator(newArray[newArray.length - 1], array[j]);
-                currentOperator = null;
-            } else {
-                newArray.push(array[j]);
-            }
-        }
-        //assigning back to the array
-        array = newArray;
-        newArray = [];
-    }
-    if (array.length > 1) {
-        console.log('Error :(');
-        return array;
-    } else {
-        return array[0];
-    }
-  }
-
-  //Parses the provided operation string into floats and operands
-  Calculator.prototype.parseOperationString = function(opString) {
-    var calculation = [];
-    var current = '';
-    var string = opString;
-        //join the op list for determining operator positions from opString
-        var operationsList = this.opList.join("");
-    for (var i = 0, ch; ch = string.charAt(i); i++) {
-      //find the indexes of al the operators present in the opString
-      var operaterPosition = operationsList.indexOf(ch);
-        if ( operaterPosition > -1) {
-            if (current == '' && ch == '-') {
-                current = '-';
-            } else {
-              if(current !== ''){
-                //number
-                calculation.push(parseFloat(current), ch);
-                current = '';
-              }else{
-                //operator
-                calculation.push(current,ch);
-              }
-            }
-        } else {
-            //No operators
-            current += string.charAt(i);
-        }
-    }
-    if (current !== '') {
-        calculation.push(parseFloat(current));
-    }
-    return calculation;
+  //console.log(stack.stack);
+  return stack;
 }
-  //Clearing out the empty values from the array
-  Calculator.prototype.clearArray =  function(array){
-    var newArray = [];
+Calculator.prototype.calculateStackItems= function(stack){
+  let currentStack = stack;
+  let tempArray = [];
+  var stackSize = stack.size();
+  //emptying the stack
+  while(stackSize>0){
+    tempArray.push(currentStack.pop());
+    stackSize = stack.size();
+  }
+  //joining the stack items together for parsing
+  var stackOpString = tempArray.reverse().join("");
+  var parsedOperationArray = this.parseOperationString(stackOpString);
+  var output = this.performOperations(parsedOperationArray);
+  return output;
+}
+Calculator.prototype.performOperations= function(array){
+  //getting the operation functions from the calculator object
+  var opList = this.opFunctions;
+  var newArray = [];
+  var currentOperator;
+  for (var i = 0; i < opList.length; i++) {
+    for (var j = 0; j < array.length; j++) {
+      if (opList[i][array[j]]) {
+        currentOperator = opList[i][array[j]];
+      } else if (currentOperator) {
+        newArray[newArray.length - 1] =
+        currentOperator(newArray[newArray.length - 1], array[j]);
+        currentOperator = null;
+      } else {
+        newArray.push(array[j]);
+      }
+    }
+    //assigning back to the array
+    array = newArray;
+    newArray = [];
+  }
+  if (array.length > 1) {
+    console.log('Error :(');
+    return array;
+  } else {
+    return array[0];
+  }
+}
+
+//Parses the provided operation string into floats and operands
+Calculator.prototype.parseOperationString = function(opString) {
+  var calculation = [];
+  var current = '';
+  var string = opString;
+  //join the op list for determining operator positions from opString
+  var operationsList = this.opList.join("");
+  for (var i = 0, ch; ch = string.charAt(i); i++) {
+    //find the indexes of al the operators present in the opString
+    var operaterPosition = operationsList.indexOf(ch);
+    if ( operaterPosition > -1) {
+      if (current == '' && ch == '-') {
+        current = '-';
+      } else {
+        if(current !== ''){
+          //number
+          calculation.push(parseFloat(current), ch);
+          current = '';
+        }else{
+          //operator
+          calculation.push(current,ch);
+        }
+      }
+    } else {
+      //No operators
+      current += string.charAt(i);
+    }
+  }
+  if (current !== '') {
+    calculation.push(parseFloat(current));
+  }
+  return calculation;
+}
+//Clearing out the empty values from the array
+Calculator.prototype.clearArray =  function(array){
+  var newArray = [];
   for (var i = 0; i < array.length; i++) {
     if (array[i] !== undefined && array[i] !== null && array[i] !== "") {
       newArray.push(array[i]);
     }
-   }
-   return newArray;
   }
-
-})();
+  return newArray;
+}
+Calculator.prototype.checkError = function(string){
+  var response = false;
+  var stringArray = string.split("");
+  var openBracketsCount=0,closedBracketCount=0;
+  stringArray.forEach(function(item){
+    if(item ==='('){
+      openBracketsCount++;
+    }
+    if(item === ')'){
+      closedBracketCount++;
+    }
+  });
+  console.log(openBracketsCount);
+  console.log(closedBracketCount);
+  response = openBracketsCount===closedBracketCount?false:true;
+  return response;
+}
--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
 (function(){
-  window.onload=function(){
-    var calculatorScreen = document.getElementById('screen');
-    var button = document.getElementsByClassName('button');
 
-    //creating a new calculator object
-    var calculator = new Calculator();
+  var calculatorScreen = document.getElementById('screen');
+  var button = document.getElementsByClassName('button');
 
-    //attaching event listners to all buttons
-    for(var i=0;i<button.length;i++){
-      button[i].addEventListener('click',handleButtonClick,false);
-    }
-    document.addEventListener('keypress',handleKeyPress,false);
+  //creating a new calculator object
+  var calculator = new Calculator();
 
-
-    //handles the button clicks
-    function handleButtonClick(event){
-      var clickedButtonValue = event.target.value;
-      var currentScreenValue = calculatorScreen.value;
-      var result = clickedButtonValue==='='?calculator.calculate(currentScreenValue):addToScreen(currentScreenValue,clickedButtonValue);
-      calculatorScreen.value = result;
-      calculatorScreen.focus();
-    }
-    function handleKeyPress(event){
-      var pressedKey = e.target.key;
-      alert(pressedKey);
-      var allowedCharacters = calculator.getAllowedCharacters();
-      var joinedAllowedChars = allowedCharacters.join("");
-      //if(joinedAllowedChars.indexOf())
-
-    }
-
+  //attaching event listners to all buttons
+  for(var i=0;i<button.length;i++){
+    button[i].addEventListener('click',handleButtonClick,false);
   }
+  document.addEventListener('keydown',handleKeyPress,false);
+
+
+  //handles the button clicks
+  function handleButtonClick(event){
+      event.target.className +=" button-clicked";
+    var clickedButtonValue = event.target.value;
+    var currentScreenValue = calculatorScreen.value;
+    var result = clickedButtonValue==='='?calculator.calculate(currentScreenValue):addToScreen(currentScreenValue,clickedButtonValue);
+    calculatorScreen.value = result;
+    calculatorScreen.focus();
+  }
+  function handleKeyPress(event){
+    var pressedKeyChar = String.fromCharCode(event.keyCode);
+    var pressedKeyCode = event.keyCode;
+    var enterKeyCode = "13";
+    var screenValue = calculatorScreen.value
+    if(pressedKeyCode===enterKeyCode){
+        calculator.calculate(screenValue);
+    }
+    //disable keys other than allowed characters
+    /*var allowedCharacters = calculator.getAllowedCharacters();
+    var joinedAllowedChars = allowedCharacters.join("");
+    if(joinedAllowedChars.indexOf(pressedKey)<0){
+      event.preventDefault();
+    }*/
+  }
+function addToScreen(currentScreenValue,clickedButtonValue){
+  return  currentScreenValue+clickedButtonValue;
+}
+
 })();
